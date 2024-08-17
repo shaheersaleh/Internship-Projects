c1f25326f7892bb9411aa3840c302377741ccf95
Shaheer <shaheer.ch143@gmail.com>
2024-08-17 12:44:41 +0500

    pls let it be final

--- PRODIGY_WD_03/tic-tac-toe/src/App.js
+++ PRODIGY_WD_03/tic-tac-toe/src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import React, { useState } from 'react';
 import LandingPage from './components/LandingPage'; 
 import Game from './components/Game'; 
